0a7513b6f2abd3f789d6cb3dcf4f2b9784077596
BouzirJawad <jawadbouzir01@gmail.com>
2025-01-18 00:09:49 +0100

    added other files from teammates

---
+++ script.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+    const backToTop = $("#backToTop");
+
+    $(window).scroll(function () {
+      if ($(this).scrollTop() > 200) {
+        backToTop.fadeIn();
+      } else {
+        backToTop.fadeOut();
+      }
+    });
+
+    backToTop.click(function () {
+      $("html, body").animate({ scrollTop: 0 }, 100);
+      return false; 
+    });
+});
