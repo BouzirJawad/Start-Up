509a6b0c2298ab43c871ea6537191eac638b084d
BouzirJawad <jawadbouzir01@gmail.com>
2025-01-18 17:56:15 +0100

    added animations to the page

--- script.js
+++ script.js
@@ -14,3 +14,56 @@ $(document).ready(function () {
       return false; 
     });
 });
+
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.utils.toArray(".rightToLeft").forEach((element) => {
+  gsap.from(element, {
+    x: "50vh",
+    opacity: 0.5,
+    duration: 2,
+    ease: "power2.out",
+    scrollTrigger: {
+        trigger: element,
+        start: "top 90%",
+        end: "top 20%",  
+        toggleActions: "play none play none",
+        scrub: 3,
+        once: true
+    }
+  });
+});
+
+gsap.utils.toArray(".leftToRight").forEach((element) => {
+  gsap.from(element, {
+    x: "-50vh",
+    opacity: 0.5,
+    duration: 2,
+    ease: "power2.out",
+    scrollTrigger: {
+        trigger: element,
+        start: "top 90%",
+        end: "top 20%",  
+        toggleActions: "play none play none",
+        scrub: 3,
+        once: true
+    }
+  });
+});
+
+gsap.utils.toArray(".bottomToTop").forEach((element) => {
+  gsap.from(element, {
+    y: "30vh",
+    opacity: 0.5,
+    duration: 2,
+    ease: "power2.out",
+    scrollTrigger: {
+        trigger: element,
+        start: "top 120%",
+        end: "top 100%",  
+        toggleActions: "play none play none",
+        scrub: 3,
+        once: true
+    }
+  });
+});
